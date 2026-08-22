d3ee7ce3c1d2ac4ea125c31430ef01dfa06a8d72
bewkun13 <jilayut.131041@gmail.com>
2026-08-22 04:36:45 -0400

    Support Vercel-prefixed Upstash variables

--- api/lib/redis.js
+++ api/lib/redis.js
@@ -1,7 +1,11 @@
 import { createHash } from 'node:crypto';
 
-const redisUrl = () => process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL;
-const redisToken = () => process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN;
+const redisUrl = () => process.env.UPSTASH_REDIS_REST_URL
+  || process.env.UPSTASH_REDIS_REST_KV_REST_API_URL
+  || process.env.KV_REST_API_URL;
+const redisToken = () => process.env.UPSTASH_REDIS_REST_TOKEN
+  || process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN
+  || process.env.KV_REST_API_TOKEN;
 
 export async function command(args) {
   const url = redisUrl(); const token = redisToken();
